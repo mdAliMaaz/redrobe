7a11c6f4a9282d3f9575e5a67fad542cd39b9db8
maaz <mdmaazi2907@gmail.com>
2025-06-03 23:35:26 +0530

    changes.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -7,10 +7,24 @@ const Hero = () => {
 	return (
 		<Container size="lg">
 			<section className="w-full relative">
+				<Image
+					src={'/images/vector_1.png'}
+					alt="vector"
+					className="absolute -bottom-5 left-0"
+					width={170}
+					height={170}
+				/>
+				<Image
+					src={'/images/vector_1.png'}
+					alt="vector"
+					className="hidden md:block absolute top-0 right-0"
+					width={200}
+					height={200}
+				/>
 				<div className="flex items-center justify-center">
 					<Heading
 						text="JOIN THE WAITLIST"
-            position='center'
+						position="center"
 						className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white z-10 font-normal"
 					/>
 					<Image src={'/images/hero-image.png'} width={1650} height={1500} alt="hero-image" />
--- src/components/subscribe.tsx
+++ src/components/subscribe.tsx
@@ -5,23 +5,30 @@ import Image from 'next/image'
 
 const Subscribe = () => {
 	return (
-		<section className="w-full bg-primary-light p-5 md:p-10">
-			<div className="w-full bg-secondary-light md:w-[70%] m-auto flex items-center gap-3 lg:gap-0 flex-col lg:flex-row">
-				<div className=" flex-1 flex flex-col gap-3 items-center justify-center p-7">
+		<section className="w-full bg-primary-light p-5 md:p-10 overflow-auto">
+			<div className="w-full bg-secondary-light md:w-[70%] m-auto flex items-center gap-3 lg:gap-0 flex-col lg:flex-row relative">
+				<Image
+					src={'/images/vector_1.png'}
+					alt="vector_1"
+					width={200}
+					height={200}
+					className=" absolute inset-0 -z-10 lg:z-10"
+				/>
+				<div className=" flex-1 flex flex-col gap-3 items-center justify-center p-2">
 					<h1 className="uppercase text-2xl">Subscribe!</h1>
 					<p className="text-gray-700 opacity-75">
 						Leave your email and get a offer updates in prior
 					</p>
 					<div className="flex items-center">
 						<input
-							className="outline-none bg-white w-full md:w-80 border-[1px] border-gray-300  px-2 py-2"
+							className="outline-none bg-white w-full md:w-80 border-[1px] border-gray-300  px-2 py-2 z-100"
 							placeholder="Email"
 						/>
 						<Button text="Subscribe" type={'button'} />
 					</div>
 				</div>
 				<div>
-					<Image src={'/images/subscribe.png'} width={400} height={400} alt="subscribe" />
+					<Image src={'/images/subscribe.png'} width={500} height={500} alt="subscribe" />
 				</div>
 			</div>
 		</section>
